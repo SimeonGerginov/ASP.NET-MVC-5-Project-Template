b03a5e413245ea54d87f7c70d6cddac736f69948
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-05 19:42:00 +0300

    Added Audit info rules

--- MVC5-Template/MVC5-Template.Persistence.Data/MsSqlDbContext.cs
+++ MVC5-Template/MVC5-Template.Persistence.Data/MsSqlDbContext.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using MVC5_Template.Core.Contracts;
 using MVC5_Template.Core.Models;
 
 namespace MVC5_Template.Persistence.Data
@@ -8,6 +14,34 @@ namespace MVC5_Template.Persistence.Data
         public MsSqlDbContext()
             : base("LocalConnection", throwIfV1Schema: false)
         {
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            var entries = this.ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is IAuditable && 
+                ((e.State == EntityState.Added) || (e.State == EntityState.Modified)));
+
+            foreach (var entry in entries)
+            {
+                var entity = (IAuditable)entry.Entity;
+
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime?))
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
         }
     }
 }
