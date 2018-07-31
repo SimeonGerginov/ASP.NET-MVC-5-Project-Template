fbf14126538d9ccdbf7569a89d7b915fc987c907
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-31 20:51:53 +0300

    Extracted Web project in separate folder

---
+++ Source/Web/MVC5-Template.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Template.Web.Startup))]
+namespace MVC5_Template.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
