3cb2475dc76cf181bc2565a5266419a25742ae79
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-31 22:26:01 +0300

    Added ViewModels for project. Fixed Account and Manage controllers errors. Fixed usings in all views.

---
+++ Source/Web/MVC5-Template.Web/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC5_Template.Web.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Source/Web/MVC5-Template.Web/ViewModels/Manage/ManageLoginsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace MVC5_Template.Web.ViewModels.Manage
+{
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
