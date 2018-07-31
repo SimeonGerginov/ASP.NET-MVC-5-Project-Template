561f997d803a07b26a4c943375c237bd449e8ebb
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-07-31 23:23:27 +0300

    Added IAuditable and IDeletable interfaces and extended User model

--- Source/Auth/MVC5-Template.Auth/Models/User.cs
+++ Source/Auth/MVC5-Template.Auth/Models/User.cs
@@ -1,14 +1,25 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
+using MVC5_Template.Core.Contracts;
+
 namespace MVC5_Template.Auth.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class User : IdentityUser
+    public class User : IdentityUser, IAuditable, IDeletable
     {
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
