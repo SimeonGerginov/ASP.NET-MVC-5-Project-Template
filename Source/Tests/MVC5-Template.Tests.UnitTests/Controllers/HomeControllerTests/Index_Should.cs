1933e7db19074517b9eed4184fcce8e3b8796bff
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-08-03 13:36:03 +0300

    Added Unit Test project

---
+++ Source/Tests/MVC5-Template.Tests.UnitTests/Controllers/HomeControllerTests/Index_Should.cs
@@ -0,0 +1,22 @@
+﻿using MVC5_Template.Web.Controllers;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace MVC5_Template.Tests.UnitTests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnDefaultView_WhenCalled()
+        {
+            // Arrange
+            var homeController = new HomeController();
+
+            // Act && Assert
+            homeController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
